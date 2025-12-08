a1ec6d323330c6175a7fea4e3e6dfe7498953810
Maria S <mariz87@yandex.ru>
2025-12-08 15:00:04 +0300

    Added changing the counter with click on the button '+1'

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ let ChangedButton=document.querySelector('.increase');
 let ClearBtn=document.querySelector('.clear');
 
 let currentCount = localStorage.getItem('counter') || 0;
+
+ChangedButton.addEventListener('click', function() {
+
+    currentCount=currentCount+1;   
+    NumberChanged.textContent = currentCount;
+
+})
