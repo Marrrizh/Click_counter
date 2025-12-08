babc4f627c868d245d05f88ddfb60efdde0b55f7
Maria S <mariz87@yandex.ru>
2025-12-08 13:56:26 +0300

    Added currentCount

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ let NumberChanged=document.querySelector('.number');
 let ChangedButton=document.querySelector('.increase');
 let ClearBtn=document.querySelector('.clear');
 
+let currentCount = localStorage.getItem('counter') || 0;
